545eb4737810384194cf0c8713df7a75e0cf5ad0
hiyad <hiyadullu05@gmail.com>
2025-03-19 16:09:07 +0530

    added login page

---
+++ pos/script.js
@@ -0,0 +1,9 @@
+// document.getElementById('textWrapper').addEventListener('mouseenter', function() {
+//     this.style.transform = "translateY(-30px)";
+//     document.getElementById('loginBtn').style.display = "block";
+// });
+
+// document.getElementById('textWrapper').addEventListener('mouseleave', function() {
+//     this.style.transform = "translateY(0)";
+//     document.getElementById('loginBtn').style.display = "none";
+// });
